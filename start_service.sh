8bf9a667d57ec0c9cce616de383904680a19399b
Siwat Jermwatthana <s1104300051612@gmail.com>
2023-10-14 01:32:52 +0700

    [Fix]: Change shell script help

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,21 @@
- #!/bin/bash
+#!/bin/bash
+display_help () {
+    printf "======================================\n"
+    printf "usages \n"
+    printf "======================================\n"
+    printf "This is a script to run docker compose\n"
+    printf "\t-h --help \t\t\t Display help. \n"
+    printf "\t-e --environment \t\t Select environment to run docker compose prod or docker compose \n"
+    printf "\tup --start  \t\t\t Run start docker compose \n"
+    printf "\tdown --stop  \t\t\t Stop docker compose \n"
+    printf "\n======================================\n"
+    printf "Examples \n"
+    printf "======================================\n"
+    printf "example#1 - use start service production:  ./start_service.sh -e=prod up \n"
+    printf "example#2 - use start service dev:  ./start_service.sh -e=dev up \n"
+    printf "example#3 - use stop service dev:  ./start_service.sh -e=dev down \n"
+    exit 1
+}
 
 while [ "$1" != "" ]
 do
@@ -7,6 +24,10 @@ do
 
     case $PARAM 
     in
+        -h|--help)
+            display_help
+            exit
+            ;;
         -e|--env-docker-compose)
             ENV_FILE="$VALUE"
             ;;
